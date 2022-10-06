86cbbdb174230868292a6ca6bdc290d8469ad0d4
swatibongu <78065014+swatibongu@users.noreply.github.com>
2022-10-06 10:05:13 +0530

    Snake and ladder game UC2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class DieRoll_UC2
+    {
+        public void SnakeLadder()
+        {
+            int position1 = 0;
+            int dieRoll = 0;
+            Console.WriteLine("Initial Position of Player 1 is :" + position1);
+
+            //Random function to roll die 1 to 6
+            Random random = new Random();
+            //range taken from 1 to 7 due to die have 1 to 6 numbers
+            dieRoll = random.Next(1, 7);
+
+            Console.WriteLine("Die Roll :" + dieRoll);
+            position1 = dieRoll;
+            Console.WriteLine("Position1 of player after dieRoll is :" + position1);
+            Console.ReadLine();
+        }
+    }
+}
