70154941805716b0b773fffab618710b80445463
swatibongu <78065014+swatibongu@users.noreply.github.com>
2022-10-06 10:00:47 +0530

    Player position zero in Snakegame.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class InitialPosition_UC1
+    {
+        public void SnakeLadder()
+        {
+            int position1 = 0;
+            Console.WriteLine("Initial Position of Player 1 is :" + position1);
+        }
+    }
+}
